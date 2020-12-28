5f2aaff00d17e602171e782328e07e3d24d114a2
Will Lewis <will@wflewis.com>
2020-12-28 11:19:38 -0500

    Clean up

--- src/crem.cr
+++ src/crem.cr
@@ -4,60 +4,56 @@ require "openssl"
 require "./gemini/*"
 
 class String
-  def puts(io : IO = STDOUT); io.puts(self); end
+  def puts(io : IO = STDOUT)
+    io.puts(self)
+  end
 end
 
 class Object
-  def pp(io : IO = STDOUT); io.puts(self.inspect); end
-  def pipe(); yield self; end
+  def pp(io : IO = STDOUT)
+    io.puts(self.inspect)
+  end
+
+  def pipe
+    yield self
+  end
 end
 
 module Crem
   VERSION = "0.1.0"
 
   module Gemini
-    VERSION = "0.14.3"
+    VERSION      = "0.14.3"
     VERSION_DATE = "2020-11-29"
   end
 end
 
 class Crem::REPL
-  @verbose = false
-
-  def verbose_puts(value)
-    if @verbose
-      puts(value)
-    end
-  end
+  @history = [] of String
 
   def start
-    loop do
-      print("uri to fetch: ")
-      STDIN.gets
-        .try(&.strip)
-        .try { |i| eval(i) }
-        .pipe { |x| x || "<no response>" }
-        .puts
-      puts("\n-------\n")
+    Signal::INT.trap do
+      puts("~~ see you! ~~")
+      exit(0)
     end
-  end
 
-  def eval(uri : String, host : String? = nil)
-    verbose_puts("establishing TLS handshake")
-    host = host || URI.parse(uri).hostname.not_nil!
-    socket = TCPSocket.new(host, 1965)
-    context = OpenSSL::SSL::Context::Client.new
-    context.verify_mode = OpenSSL::SSL::VerifyMode::NONE
-    ssl_socket = OpenSSL::SSL::Socket::Client.new(socket, context, hostname: host)
-
-    verbose_puts("making gemini request")
-    ssl_socket << "#{uri}\r\n"
-    ssl_socket.flush
-    ssl_socket.gets_to_end
+    Crem::Gemini::Client.open do |client|
+      loop do
+        print("uri to fetch: ")
+        if line = STDIN.gets
+          @history << line
+          response = client.fetch(line.strip)
+          case response
+          when Gemini::Response::Success then puts("success")
+          end
+          puts("\n-------\n")
+        end
+      end
+    end
   end
 end
 
 case ARGV[0]?
 when "repl" then Crem::REPL.new.start
-else STDERR.puts("usage: #{PROGRAM_NAME} repl"); exit(1)
+else             STDERR.puts("usage: #{PROGRAM_NAME} repl"); exit(1)
 end
---
+++ src/gemini/client.cr
@@ -0,0 +1,32 @@
+class Crem::Gemini::Client
+  def self.open
+    yield self.new
+  end
+
+  def fetch(uri) : Crem::Gemini::Response
+    host = URI.parse(uri).hostname.not_nil!
+    socket = TCPSocket.new(host, 1965)
+    context = OpenSSL::SSL::Context::Client.new
+    context.verify_mode = OpenSSL::SSL::VerifyMode::NONE
+    ssl_socket = OpenSSL::SSL::Socket::Client.new(socket, context, hostname: host)
+
+    ssl_socket << "#{uri}\r\n"
+    ssl_socket.flush
+    Crem::Gemini::Response.parse(ssl_socket)
+  end
+end
+
+module Crem::Gemini
+  abstract class Response
+    def self.parse(io : IO) : Response
+      case io.gets(2).try &.to_i32
+      when 20 then Success.new(io)
+      else         raise "invalid"
+      end
+    end
+
+    class Success < Response
+      def initialize(io); end
+    end
+  end
+end
