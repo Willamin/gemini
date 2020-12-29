fe98ae3554033b66e1cec61271cf416f93fc938a
Will Lewis <will@wflewis.com>
2020-12-28 23:37:48 -0500

    Flesh out hard-coded server

--- src/crem/server.cr
+++ src/crem/server.cr
@@ -8,8 +8,8 @@ class Crem::Server
 
     tcp_server = TCPServer.new("0.0.0.0", 1965)
     ssl_context = OpenSSL::SSL::Context::Server.new
-    ssl_context.certificate_chain = "openssl.crt"
-    ssl_context.private_key = "openssl.key"
+    ssl_context.certificate_chain = "cert.pem"
+    ssl_context.private_key = "key.pem"
     ssl_server = OpenSSL::SSL::Server.new(tcp_server, ssl_context)
 
     while client = ssl_server.accept?
@@ -18,7 +18,8 @@ class Crem::Server
   end
 
   def handle_client(client)
-    client.puts "hello"
+    client << "20 text/gemini; charset=utf-8\r\nhello world\njust a quick, hard-coded gemini response; more to come"
+    client.close
   end
 end
 
