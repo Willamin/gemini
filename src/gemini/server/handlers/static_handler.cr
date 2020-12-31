f2a17d3d83f51ed4a60ea66cf7421d15abcb4afc
Will Lewis <will@wflewis.com>
2020-12-31 10:37:21 -0500

    Allow StaticHandler to fallthrough with an option

--- src/gemini/server/handlers/static_handler.cr
+++ src/gemini/server/handlers/static_handler.cr
@@ -1,7 +1,7 @@
 class Gemini::Server::StaticHandler
   include Gemini::Server::Handler
 
-  def initialize(@root : String); end
+  def initialize(@root : String, @fallthrough = false); end
 
   def call(context)
     path = Path.new(@root, context.request.uri.path)
@@ -12,8 +12,11 @@ class Gemini::Server::StaticHandler
       context.response.status = Gemini::Status::Success
       context.response.print File.read(path)
     else
-      context.response.status = Gemini::Status::NotFound
-      call_next(context)
+      if @fallthrough
+        call_next(context)
+      else
+        context.response.status = Gemini::Status::NotFound
+      end
     end
   end
 end
